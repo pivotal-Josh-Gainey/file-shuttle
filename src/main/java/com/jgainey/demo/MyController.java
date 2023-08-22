e083e4ef11fe0e41521b20a1ba99510ea0ee2314
jgainey <jgainey@pivotal.io>
2023-08-22 16:03:20 -0400

    An app to shuttle a file around

---
+++ src/main/java/com/jgainey/demo/MyController.java
@@ -0,0 +1,77 @@
+package com.jgainey.demo;
+
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.io.*;
+@Controller
+public class MyController {
+
+    String fileName = "";
+    String fullFilePath = "";
+
+    @RequestMapping(method = RequestMethod.POST, value="/upload", consumes = {"multipart/form-data"},produces = "application/json")
+    public ResponseEntity<String> importFast(@Valid @RequestParam("file") MultipartFile multiPartFile) {
+
+        InputStream initialStream = null;
+        try {
+            initialStream = multiPartFile.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        byte[] buffer = new byte[0];
+        try {
+            buffer = new byte[initialStream.available()];
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            initialStream.read(buffer);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        fileName = multiPartFile.getOriginalFilename();
+        fullFilePath = "/tmp/" + fileName;
+
+        File targetFile = new File(fullFilePath);
+
+        try (OutputStream outStream = new FileOutputStream(targetFile)) {
+            try {
+                outStream.write(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/download", method = RequestMethod.GET)
+    public void download(HttpServletResponse response) {
+
+        InputStream inputStream = null;
+        response.setHeader("Content-Disposition","attachment; filename="+fileName);
+        try {
+            inputStream = new FileInputStream(new File(fullFilePath));
+            IOUtils.copy(inputStream, response.getOutputStream());
+            response.flushBuffer();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
